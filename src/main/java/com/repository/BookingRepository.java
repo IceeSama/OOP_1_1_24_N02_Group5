a003e87b921ae8155acf8642118ed08bca4bd4b9
NguyenMinhDuc22010286 <22010286@st.phenikaa-uni.edu.vn>
2024-10-18 13:35:59 +0700

    a

--- src/main/java/com/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yourappname.repository;
-
-import com.yourappname.entity.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingRepository extends JpaRepository<Booking, Integer> {
-}
